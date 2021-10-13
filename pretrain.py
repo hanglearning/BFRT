4250575eb5b21ef7f42b3b32d66c431c9638de17
Hang Chen <hangforjobs@gmail.com>
2021-10-13 16:20:11 -0400

    add resume to pretrain

--- pretrain.py
+++ pretrain.py
@@ -22,6 +22,7 @@ import tensorflow as tf
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="traffic_fedavg_simulation")
 
+parser.add_argument('-rp', '--resume_path', type=str, default=None, help='provide the last pretrained h5 file path to continue pretraining')
 parser.add_argument('-il', '--input_length', type=int, default=12, help='input length for the LSTM network')
 parser.add_argument('-dp', '--data_path', type=str, default='/content/drive/MyDrive/Traffic Prediction FedAvg Simulation/traffic_data/Preprocessed_V1.1_4sensors', help='dataset path')
 parser.add_argument('-p', '--pretrain_percentage', type=float, default=1.00, help='MUST SPECIFY `pre_train_percentage` to be non-0 for the pretrain function to run.')
@@ -34,22 +35,36 @@ args = args.__dict__
 
 dataset_path = args['data_path']
 
-model_chosen = args['model']
-if model_chosen == 'lstm':
-    build_model = build_lstm
-elif model_chosen == 'gru':
-    build_model = build_gru
+if args["resume_path"]:
+    with open(f"{args["resume_path"]}/vars_record.pkl", 'wb') as f:
+		vars_record = pickle.loadf)
+    model_chosen = vars_record['model']
+    pretrain_config = vars_record['pretrain_config']
+    pretrain_percentage= vars_record['pretrain_percentage']
+    INPUT_LENGTH = vars_record['INPUT_LENGTH']
 else:
-    sys.exit(f"Model specification error - must be 'lstm' or 'gru', but got {args['model']}.")
+    vars_record = {}
+    model_chosen = args['model']
+    vars_record['model'] = model_chosen
+    if model_chosen == 'lstm':
+        build_model = build_lstm
+    elif model_chosen == 'gru':
+        build_model = build_gru
+    else:
+        sys.exit(f"Model specification error - must be 'lstm' or 'gru', but got {args['model']}.")
     
-pretrain_config = {"batch": args['batch'], "epochs":  args['epoch']}
-pretrain_percentage = args['pretrain_percentage']
-INPUT_LENGTH = args['input_length']
-
-# create log folder indicating by current running date and time
-date_time = datetime.now().strftime("%m%d%Y_%H%M%S")
-log_files_folder_path = f"/content/drive/MyDrive/Traffic Prediction FedAvg Simulation/device_outputs_Preprocessed_V1.1/{date_time}_pretrain_{model_chosen}"
-os.makedirs(log_files_folder_path, exist_ok=True)
+    pretrain_config = {"batch": args['batch'], "epochs":  args['epoch']}
+    pretrain_percentage = args['pretrain_percentage']
+    INPUT_LENGTH = args['input_length']
+    vars_record['pretrain_config'] = pretrain_config
+    vars_record['pretrain_percentage'] = pretrain_percentage
+    vars_record['INPUT_LENGTH'] = INPUT_LENGTH
+    # create log folder indicating by current running date and time
+    date_time = datetime.now().strftime("%m%d%Y_%H%M%S")
+    log_files_folder_path = f"/content/drive/MyDrive/Traffic Prediction FedAvg Simulation/device_outputs_Preprocessed_V1.1/{date_time}_pretrain_{model_chosen}"
+    os.makedirs(log_files_folder_path, exist_ok=True)
+    with open(f"{log_files_folder_path}/vars_record.pkl", 'wb') as f:
+		pickle.dump(vars_record, f)
 
 def build_pretrain_dataset(pretrain_percentage, INPUT_LENGTH, all_sensor_files, dataset_path):
   
@@ -100,11 +115,7 @@ def pretrain_model(model_path, X_train, y_train, log_files_folder_path, epoch, s
     # loss_df.to_csv(f'{log_files_folder_path}/loss.csv', encoding='utf-8', index=False)
     return model_file_path
 
-def run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, all_sensor_files, dataset_path, INPUT_LENGTH):
-  with open(f'{log_files_folder_path}/pretrain_config.txt', 'w+') as config_file:
-    config_file.write(f"pretrain_config: {repr(pretrain_config)} + \n")
-    config_file.write(f"INPUT_LENGTH: {repr(INPUT_LENGTH)} + \n")
-    config_file.write(f"pretrain_percentage: {repr(pretrain_percentage)} + \n")
+def run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, all_sensor_files, dataset_path, INPUT_LENGTH, args["resume_path"]):
   # build pretrain_dataset
   pretrain_datasets, post_pretrain_data_index = build_pretrain_dataset(pretrain_percentage, INPUT_LENGTH, all_sensor_files, dataset_path)
   # process data
@@ -117,16 +128,26 @@ def run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, al
   # build pretrain model
   model_to_pretrain = build_model([INPUT_LENGTH, 64, 64, 1])
   model_to_pretrain.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
-  model_file_path = f'{log_files_folder_path}/pretrain_0.h5'
-  model_to_pretrain.save(model_file_path)
+  if not args["resume_path"]:
+    model_file_path = f'{log_files_folder_path}/pretrain_0.h5'
+    model_to_pretrain.save(model_file_path)
+    starting_epoch = 1
+    seq = 1
+  else:
+      [_, starting_epoch, seq] = args["resume_path"].split(".")[-2].split("_")
+      if seq == len(processed_pretrain_datasets):
+          starting_epoch += 1
+          seq = 1
   # begin training
-  for epoch in range(1, pretrain_config["epochs"] + 1):
-      seq = 1
-      for processed_pretrain_dataset in processed_pretrain_datasets: # instead of csv concatenation
+  for epoch in range(starting_epoch, pretrain_config["epochs"] + 1):
+      iter_control = range(seq - 1, len(processed_pretrain_datasets))
+      for dataset_iter in iter_control:
+          processed_pretrain_dataset = processed_pretrain_datasets[dataset_iter]
           (sensor_file, X_train, y_train) = processed_pretrain_dataset
           print(f"Training epoch {epoch} csv file {seq}/{len(processed_pretrain_datasets)} {sensor_file}...")
           model_file_path = pretrain_model(model_file_path, X_train, y_train, log_files_folder_path, epoch, seq, pretrain_config["batch"])
           seq += 1
+      seq = 1
 
   post_pretrain_data_index_saved_path = f'{log_files_folder_path}/post_pretrain_data_index.pkl'
   with open(post_pretrain_data_index_saved_path, 'wb') as f:
@@ -137,4 +158,4 @@ def run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, al
 # Import data files (csv)
 all_sensor_files = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f)) and '.csv' in f]
 # build pretrain dataset and model
-run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, all_sensor_files, dataset_path, INPUT_LENGTH)
+run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, all_sensor_files, dataset_path, INPUT_LENGTH, args["resume_path"])
