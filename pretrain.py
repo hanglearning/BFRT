dce6befd809ae956c471558ee74fef0410bb3568
Hang Chen <hangforjobs@gmail.com>
2021-10-13 15:34:19 -0400

    should have fixed issues caused by pretrain csv concatenation

--- pretrain.py
+++ pretrain.py
@@ -16,6 +16,7 @@ from build_gru import build_gru
 from process_data import process_pretrain_data
 import argparse
 
+from keras.models import load_model
 import tensorflow as tf
 
 
@@ -70,10 +71,11 @@ def build_pretrain_dataset(pretrain_percentage, INPUT_LENGTH, all_sensor_files, 
     post_pretrain_data_index[sensor_file] = data_index
     pretrain_datasets.append(pretrain_data)
 
-  return pd.concat(pretrain_datasets, axis=0), post_pretrain_data_index
+  # return pd.concat(pretrain_datasets, axis=0), post_pretrain_data_index
+  return pretrain_datasets, post_pretrain_data_index
 
 # pretrain the model (part of the train_model())
-def pretrain_model(model, X_train, y_train, log_files_folder_path, pretrain_config):
+def pretrain_model(model_path, X_train, y_train, log_files_folder_path, epoch, seq, batch):
     """train
     train a single model.
 
@@ -83,27 +85,16 @@ def pretrain_model(model, X_train, y_train, log_files_folder_path, pretrain_conf
         y_train: ndarray(number, ), label data for train.
         log_files_folder_path: specify directory to store log files
         pretrain_config: Dict, parameter for train.
-    """
-
-    model.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
-    model_checkpoint_path = f'{log_files_folder_path}/pretrain_checkpoint'
-    model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-      filepath=model_checkpoint_path,
-      save_weights_only=True,
-      monitor='val_accuracy',
-      mode='max',
-      save_best_only=True,
-      save_freq='epoch',
-      period=10)
+    """    
+    model = load_model(model_path)
     hist = model.fit(
         X_train, y_train,
-        batch_size=pretrain_config["batch"],
-        epochs=pretrain_config["epochs"],
+        batch_size=batch,
+        epochs=1,
         validation_split=0.05)
     
-    
     # save model weights and loss to file
-    model_file_path = f'{log_files_folder_path}/pretrain.h5'
+    model_file_path = f'{log_files_folder_path}/pretrain_{epoch}_{seq}.h5'
     model.save(model_file_path)
     # loss_df = pd.DataFrame.from_dict(hist.history)
     # loss_df.to_csv(f'{log_files_folder_path}/loss.csv', encoding='utf-8', index=False)
@@ -115,20 +106,31 @@ def run_pretrain(log_files_folder_path, pretrain_config, pretrain_percentage, al
     config_file.write(f"INPUT_LENGTH: {repr(INPUT_LENGTH)} + \n")
     config_file.write(f"pretrain_percentage: {repr(pretrain_percentage)} + \n")
   # build pretrain_dataset
-  pretrain_dataset, post_pretrain_data_index = build_pretrain_dataset(pretrain_percentage, INPUT_LENGTH, all_sensor_files, dataset_path)
+  pretrain_datasets, post_pretrain_data_index = build_pretrain_dataset(pretrain_percentage, INPUT_LENGTH, all_sensor_files, dataset_path)
   # process data
-  X_train, y_train = process_pretrain_data(pretrain_dataset, INPUT_LENGTH)
-  X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+  processed_pretrain_datasets = []
+  for pretrain_dataset in pretrain_datasets:
+    X_train, y_train = process_pretrain_data(pretrain_dataset, INPUT_LENGTH)
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    processed_pretrain_datasets.append((X_train, y_train))
   # build pretrain model
   model_to_pretrain = build_model([INPUT_LENGTH, 64, 64, 1])
+  model_to_pretrain.compile(loss="mse", optimizer="rmsprop", metrics=['mape'])
+  model_file_path = f'{log_files_folder_path}/pretrain_0.h5'
+  model_to_pretrain.save(model_file_path)
   # begin training
-  pretrained_model_file_path = pretrain_model(model_to_pretrain, X_train, y_train, log_files_folder_path, pretrain_config)
+  for epoch in range(1, pretrain_config["epochs"] + 1):
+      seq = 1
+      for processed_pretrain_dataset in processed_pretrain_datasets: # instead of csv concatenation
+          X_train, y_train = processed_pretrain_dataset[0], processed_pretrain_dataset[1]
+          model_file_path = pretrain_model(model_file_path, X_train, y_train, log_files_folder_path, epoch, seq, pretrain_config["batch"])
+          seq += 1
 
   post_pretrain_data_index_saved_path = f'{log_files_folder_path}/post_pretrain_data_index.pkl'
   with open(post_pretrain_data_index_saved_path, 'wb') as f:
       pickle.dump(post_pretrain_data_index, f)
 
-  print(f"The path to the file of this pretrained model is located at {pretrained_model_file_path}")
+  print(f"The path to the file of this pretrained model is located at {model_file_path}")
 
 # Import data files (csv)
 all_sensor_files = [f for f in listdir(dataset_path) if isfile(join(dataset_path, f)) and '.csv' in f]
