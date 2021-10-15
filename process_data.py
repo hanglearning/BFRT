76b30a893305364c698cc84ac1ca0375029422f8
Hang Chen <hangforjobs@gmail.com>
2021-10-15 12:22:21 -0400

    cannot use 'if dataframe'

--- process_data.py
+++ process_data.py
@@ -54,29 +54,27 @@ def process_data(df_train, df_test, is_chained, INPUT_LENGTH):
     scaler = MinMaxScaler(feature_range=(0, 1)).fit(df_train[attr].values.reshape(-1, 1))
     X_train, y_train, X_test, y_test = None, None, None, None
 
-    if df_train:
-        flow_train = scaler.transform(df_train[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
-        train_set = []
-        for i in range(INPUT_LENGTH, len(flow_train)):
-            train_set.append(flow_train[i - INPUT_LENGTH: i + 1])
-        train = np.array(train_set)
-        X_train = train[:, :-1]
-        y_train = train[:, -1]
+    flow_train = scaler.transform(df_train[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    train_set = []
+    for i in range(INPUT_LENGTH, len(flow_train)):
+        train_set.append(flow_train[i - INPUT_LENGTH: i + 1])
+    train = np.array(train_set)
+    X_train = train[:, :-1]
+    y_train = train[:, -1]
     
-    if df_test:
-        flow_test = scaler.transform(df_test[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
-        test_set = []
-        if is_chained:
-            test_set.append(flow_test)
-        else:
-            for i in range(INPUT_LENGTH, len(flow_test)):
-                test_set.append(flow_test[i - INPUT_LENGTH: i + 1])
-        test = np.array(test_set) 
-        if is_chained:
-            X_test = None
-            y_test = test.flatten()
-        else:
-            X_test = test[:, :-1]
-            y_test = test[:, -1]
+    flow_test = scaler.transform(df_test[attr].values.reshape(-1, 1)).reshape(1, -1)[0]
+    test_set = []
+    if is_chained:
+        test_set.append(flow_test)
+    else:
+        for i in range(INPUT_LENGTH, len(flow_test)):
+            test_set.append(flow_test[i - INPUT_LENGTH: i + 1])
+    test = np.array(test_set) 
+    if is_chained:
+        X_test = None
+        y_test = test.flatten()
+    else:
+        X_test = test[:, :-1]
+        y_test = test[:, -1]
 
     return X_train, y_train, X_test, y_test, scaler
